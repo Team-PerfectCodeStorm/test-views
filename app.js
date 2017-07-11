5240c83f298b6fe0aeb98895f4c298eb1d5250ad
Yask00 <jasstreb@abv.bg>
2017-07-11 13:48:06 +0300

    setup

--- app.js
+++ app.js
@@ -9,9 +9,23 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
+// MAIN PAGE
 app.get('/', function(req, res) {
     res.render('index');
 });
+// LIST ALL PAGE
+app.get('/all', function(req, res) {
+    res.render('all');
+});
+// LOGIN PAGE
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+//REGISTER PAGE
+app.get('/register', function(req, res) {
+    res.render('register');
+});
+
 
 app.listen(port, () => {
     console.log(`App started on localhost:${port}`);
